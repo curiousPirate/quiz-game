a75c4423495f636525b673237176441e5c269d47
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-03-19 02:32:55 +0530

    added questions

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,53 @@
+//Source for questions : https://www.interviewbit.com/javascript-mcq/
+const questions = [
+  {
+  question: "Javascript is an _______ language?",
+  options: ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+  answer: "Object-Oriented"
+},
+{
+  question: "Which of the following methods is used to access HTML elements using Javascript?",
+  options: ["getElementbyId()", "getElementbyClassName()", "Both A & B", "None of the above"],
+  answer: "Both A & B"
+},
+{
+  question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+  options: ["Throws an error", "Ignore the statement", "Gives a warning", "None of the above"],
+  answer: "Ignore the statement"
+},
+{
+  question: "Which of the following methods can be used to display data in some form using Javascript?",
+  options: ["console.log()", "window.alert()", "document.write()", "All the above"],
+  answer: "All the above"
+},
+{
+  question: "What keyword is used to check whether a given property is valid or not?",
+  options: ["in", "is in", "exists", "lies"],
+  answer: "in"
+},
+{
+  question: "When an operator's value is NULL, the typeof returned by the unary operator is:",
+  options: ["Boolean", "Undefined", "Object", "Integer"],
+  answer: "Object"
+},
+{
+  question: "Which of the following are closures in Javascript?",
+  options: ["Object", "Variables", "Functions", "All the above"],
+  answer: "All the above"
+},
+{
+  question: "Which of the following is not a Javascript framework?",
+  options: ["Node", "React", "Cassandra", "Vue"],
+  answer: "Cassandra"
+},
+{
+  question: "What keyword is used to declare an asynchronous function in Javascript?",
+  options: ["async", "await", "setTimeout", "None of the above"],
+  answer: "async"
+},
+{
+  question: "How to stop an interval timer in Javascript?",
+  options: ["clearInterval", "clearTime", "intervalOver", "None of the above"],
+  answer: "clearInterval"
+}
+]
