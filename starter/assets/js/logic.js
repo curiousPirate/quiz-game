6a9e46ff1ef088e05e0c353b43c299b6f69780f7
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-03-20 02:11:20 +0530

    added end game function

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,4 +1,4 @@
-//Selecting all DOM elements
+// Selecting all DOM elements
 const timerEl = document.querySelector("#time")
 const startEl = document.querySelector("#start-screen")
 const startBtn = document.getElementById("start")
@@ -9,86 +9,80 @@ const endScreenEl = document.querySelector("#end-screen")
 const finalScoreEl = document.querySelector("#final-score")
 const userName = document.querySelector("#initials")
 const submitBtn = document.querySelector("#submit")
-const feedBackEl = document.querySelector("#feedback")
+const feedbackEl = document.querySelector("#feedback")
 
-//Timer set to 100 secs
-var secondsLeft = 100
 
-//User input records
-var correctAnswer = 0
-var wrongAnswer = 0
+// Timer set to 100 seconds
+var secondsLeft = 100;
+var timerInterval;
 
-//Timer count down function
+// Timer countdown function
 function startCountdown() {
   timerInterval = setInterval(function () {
-    secondsLeft--
-    timerEl.textContent = secondsLeft
+    secondsLeft--;
+    timerEl.textContent = secondsLeft;
     if (secondsLeft === 0) {
-      clearInterval(timerInterval)
+      clearInterval(timerInterval);
+      endQuiz();
     }
-  }, 1000)
+  }, 1000);
 }
 
-//Start quiz function
-function startQuiz () {
-  startEl.classList.add("hide")
-  questionsEl.classList.remove("hide")
-  feedBackEl.classList.remove("hide")
-  displayQuestions()
-  startCountdown()
+// Start quiz function
+function startQuiz() {
+  startEl.classList.add("hide");
+  questionsEl.classList.remove("hide");
+  feedbackEl.classList.remove("hide");
+  displayQuestion();
+  startCountdown();
 }
 
-//Start btn event listener
-startBtn.addEventListener("click", startQuiz)
+let currentQuestionIndex = 0;
 
-// function displayQuestions() {
-//   for(i = 0; i < questions.length; i++) {
-//     questionTitleEl.textContent = questions[i].question
+function displayQuestion() {
+  const currentQuestion = questions[currentQuestionIndex];
+  questionTitleEl.textContent = currentQuestion.question;
+  choicesEl.innerHTML = "";
 
-//     // Clear the existing choice buttons
-//     choicesEl.innerHTML = ""
-
-//     // Create a button element for each option and append it to the choices element
-//     for (j = 0; j < questions[i].options.length; j++) {
-//       let choiceBtn = document.createElement("button")
-//       choiceBtn.textContent = questions[i].options[j]
-//       choicesEl.appendChild(choiceBtn)
-//       choicesEl.appendChild(document.createElement("br"))
-//       choiceBtn.style.marginBottom = "10px" 
-//     }
-//   }
-// }
-
-function displayQuestions() {
-  for (let i = 0; i < questions.length; i++) {
-    // Set the question title
-    questionTitleEl.textContent = questions[i].question
-
-    // Clear the choices container
-    choicesEl.innerHTML = ""
-
-    // Create a button for each choice and append it to the choices container
-    for (let j = 0; j < questions[i].options.length; j++) {
-      const choiceBtn = document.createElement("button")
-      choiceBtn.innerHTML = questions[i].options[j]
-      choiceBtn.classList.add("choice")
-      choicesEl.appendChild(choiceBtn)
-
-      // Add an event listener to the button
-      choiceBtn.addEventListener("click", function () {
-        // Check if the user's response is correct
-        const userResponse = choiceBtn.innerHTML
-        const correctAnswer = questions[i].answer
-        if (userResponse === correctAnswer) {
-          correctAnswer++
-          feedBackEl.textContent = "Correct!"
-        } else {
-          wrongAnswer++
-          feedBackEl.textContent = "Wrong!"
+  currentQuestion.options.forEach((option) => {
+    const choiceBtn = document.createElement("button");
+    choiceBtn.innerHTML = option;
+    choicesEl.appendChild(choiceBtn);
+    choiceBtn.addEventListener("click", function () {
+      if (option === currentQuestion.answer) {
+        feedbackEl.textContent = "Correct!";
+      } else {
+        feedbackEl.textContent = "Wrong!";
+        secondsLeft -= 10;
+        if (secondsLeft < 0) {
+          secondsLeft = 0;
         }
-      });
-    }
-  }
+      }
+      currentQuestionIndex++;
+      if (currentQuestionIndex === questions.length) {
+        endQuiz();
+      } else {
+        displayQuestion();
+      }
+    });
+  });
 }
 
 
+// End quiz function
+function endQuiz() {
+  clearInterval(timerInterval);
+  questionsEl.classList.add("hide");
+  endScreenEl.classList.remove("hide");
+  finalScoreEl.textContent = secondsLeft;
+}
+
+// Submit score function
+submitBtn.addEventListener("click", function () {
+  localStorage.setItem("initials", userName.value);
+  localStorage.setItem("score", secondsLeft);
+  window.location.href = "highscores.html";
+});
+
+// Start quiz event listener
+startBtn.addEventListener("click", startQuiz);
